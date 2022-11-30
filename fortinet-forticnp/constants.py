f5e1e44e335c952c9689a869bb6a79dd4f8e5d6e
cs-pranjali-munfan <pmunfan@fortinet.com>
2022-11-30 10:51:21 +0530

    Update constants.py

--- fortinet-forticnp/constants.py
+++ fortinet-forticnp/constants.py
@@ -8,6 +8,8 @@ GET_FINDING_LIST = '/api/v2/finding/list'
 GET_ACCOUNT_SEVERITY_LEVEL = '/api/v1/dashboard/cloud/account/list'
 GET_ALERT_SEVERITY = '/api/v1/severity'
 GET_RESOURCE_MAP = '/api/v1/resourceURLMap'
+AUTH_API = '/api/v1/auth/credentials/token/'
+GET_ALERTS = '/api/v1/alert/list'
 
 PARAM_MAPPING_DICT = {
     "ID": "id",
