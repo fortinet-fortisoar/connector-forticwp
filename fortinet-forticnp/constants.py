d88cc34173e2c9ed2dd94c3155dc5096f0919cb5
cs-shubham-morankar <smorankar@fortinet.com>
2022-12-23 12:02:57 +0530

    Bug Fixed and Removed unrequired actions

--- fortinet-forticnp/constants.py
+++ fortinet-forticnp/constants.py
@@ -1,15 +1,14 @@
-GET_RESOURCE_CONFIG = '/api/workload/v2/profile/resource/tableView/configAssess'
+""" Copyright start
+  Copyright (C) 2008 - 2022 Fortinet Inc.
+  All rights reserved.
+  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
+  Copyright end """
+
 GET_RESOURCE_LIST = '/api/workload/v1/profile/resource/list'
 GET_RESOURCE_DETAILS = '/api/workload/v1/profile/resource/topology/detail'
-GET_EVENTS = '/api/v1/event'
-GET_POLICY_VIOLATION = '/api/v1/dashboard/policy/violation'
-GET_DOCUMENT_VIOLATION = '/api/v1/profile/document/violation'
 GET_FINDING_LIST = '/api/v2/finding/list'
-GET_ACCOUNT_SEVERITY_LEVEL = '/api/v1/dashboard/cloud/account/list'
-GET_ALERT_SEVERITY = '/api/v1/severity'
 GET_RESOURCE_MAP = '/api/v1/resourceURLMap'
 AUTH_API = '/api/v1/auth/credentials/token/'
-GET_ALERTS = '/api/v1/alert/list'
 
 PARAM_MAPPING_DICT = {
     "ID": "id",
